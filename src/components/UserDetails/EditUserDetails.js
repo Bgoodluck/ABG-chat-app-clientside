ff55ec8a5b9fdf97aa85f03d2d661bc57b252a20
unknown <Bgoodluck>
2024-12-30 15:03:17 +0100

    retry

--- src/components/UserDetails/EditUserDetails.js
+++ src/components/UserDetails/EditUserDetails.js
@@ -220,6 +220,7 @@ function EditUserDetails({ onClose, user }) {
   };
 
   
+  
 
   return (
     <div className="fixed top-0 bottom-0 left-0 right-0 z-50 bg-gray-700 bg-opacity-40 flex justify-center items-center">
